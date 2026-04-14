b68906a27ea852752d8280209d8fff34c38cc0a0
Dhiraj Khandare <dhirajkhandare76@gmail.com>
2026-04-14 22:48:26 +0530

    Add files via upload

---
+++ video 07/MyFirstApp/src/main/java/com/example/MyFirstApp/Car.java
@@ -0,0 +1,16 @@
+package com.example.MyFirstApp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Car {
+    @Autowired
+    private Dog dog;
+
+    @GetMapping("/abc")
+    public String sayHello(){
+        return dog.fun();
+    }
+}
---
+++ video 07/MyFirstApp/src/main/java/com/example/MyFirstApp/Dog.java
@@ -0,0 +1,10 @@
+package com.example.MyFirstApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog {
+    public String fun(){
+        return "Bark on thief";
+    }
+}
